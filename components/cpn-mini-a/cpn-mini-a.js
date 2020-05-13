4c0509d0b3aa081f13a5b6e93ecd8b4195da6cae
PC-20170405RXBQ\Administrator <384014886@qq.com>
2020-05-13 17:07:04 +0800

    组件

---
+++ components/cpn-mini-a/cpn-mini-a.js
@@ -0,0 +1,13 @@
+// components/cpn-mini-a/cpn-mini-a.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    btn: String,
+    content: {
+      type: String,
+      value: "组件内容"
+    }
+  }
+})
